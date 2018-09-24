642a12bc2df4a6aa12a8a940ef22b9455e4dcdff
Leo espinoza <leo.spinoza@yahoo.com>
2018-09-24 12:13:35 -0500

    added the actions components

--- client/src/components/layout/sample.js
+++ client/src/components/layout/sample.js
@@ -9,3 +9,4 @@ export default class sample extends Component {
     )
   }
 }
+ 
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,14 +1,36 @@
+
+
+
+import {TEST_DISPATCH} from '../actions/types'
+
+
+
+
+
+
+
+
+
 // import isEmpty from '../validation/is-empty';
 
 // import { SET_CURRENT_USER } from '../actions/types';
 
 const initialState = {
   isAuthenticated: false,
-  user: {}
+  user: {},
+  text: 'Hello'
 };
 
 export default function(state = initialState, action) {
   switch (action.type) {
+
+    case TEST_DISPATCH:
+      return {
+        ...state,
+          user: action.payload
+      }
+
+
     // case SET_CURRENT_USER:
     //   return {
     //     ...state,
